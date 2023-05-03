5f703cd0f0af52203fd5bb1fd6ec6945109b1925
JuraGrzegorz <jura.g@o2.pl>
2023-05-03 22:39:38 +0200

    wykonane

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -2,9 +2,12 @@ package edu.kis.vh.nursery;
 
 public class defaultCountingOutRhymer {
 
-    private int[] NUMBERS = new int[12];
+    public static final int MAX_SIZE = 12;
+    public static final int EMPTY = -1;
+    public static final int FULL = 11;
+    private int[] NUMBERS = new int[MAX_SIZE];
 
-    public int total = -1;
+    public int total = EMPTY;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +15,22 @@ public class defaultCountingOutRhymer {
     }
 
         public boolean callCheck() {
-            return total == -1;
+            return total == EMPTY;
         }
         
             public boolean isFull() {
-                return total == 11;
+                return total == FULL;
             }
         
                 protected int peekaboo() {
                     if (callCheck())
-                        return -1;
+                        return EMPTY;
                     return NUMBERS[total];
                 }
             
                     public int countOut() {
                         if (callCheck())
-                            return -1;
+                            return EMPTY;
                         return NUMBERS[total--];
                     }
 
