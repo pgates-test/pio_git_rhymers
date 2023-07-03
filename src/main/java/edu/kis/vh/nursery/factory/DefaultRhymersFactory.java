fa3084b5f9306a9c61b8d43666b26e752018d7d8
user1 <bramowskip@gmail.com>
2023-07-03 20:20:05 +0200

    zad 17

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -4,26 +4,45 @@ import edu.kis.vh.nursery.DefaultCountingOutRhymer;
 import edu.kis.vh.nursery.FIFORhymer;
 import edu.kis.vh.nursery.HanoiRhymer;
 
+
+/**
+ * Default class implementing Rhymers Factory interface
+ */
 public class DefaultRhymersFactory implements RhymersFactory {
 
+    /**
+     * Method creating Standard Rhymer
+     * @return New Standard Rhymer
+     */
     @Override
     public DefaultCountingOutRhymer getStandardRhymer() {
         return new DefaultCountingOutRhymer();
     }
 
+    /**
+     * Method creating False Rhymer
+     * @return New False Rhymer
+     */
     @Override
     public DefaultCountingOutRhymer getFalseRhymer() {
         return new DefaultCountingOutRhymer();
     }
 
+    /**
+     * Method creating FIFO Rhymer
+     * @return New FIFO Rhymer
+     */
     @Override
     public DefaultCountingOutRhymer getFIFORhymer() {
         return new FIFORhymer();
     }
 
+    /**
+     * Method creating Hanoi Rhymer
+     * @return New Hanoi Rhymer
+     */
     @Override
     public DefaultCountingOutRhymer getHanoiRhymer() {
         return new HanoiRhymer();
     }
-
 }
