9e8768b5e3eb7ad8432cb5b1d911b6520190ebbf
user1 <bramowskip@gmail.com>
2023-07-03 19:37:59 +0200

    zad 10

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -4,8 +4,10 @@ public class DefaultCountingOutRhymer {
 
     private int[] numbers = new int[12];
 
-    public int total = -1;
-
+    private int total = -1;
+    public int getTotal() {
+        return total;
+    }
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
